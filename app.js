55497001f84e7b93069a90ab63b906d3e05d00d5
Marcus Goncalves <marcuspgk@gmail.com>
2021-07-21 19:17:00 -0300

    adicao de projeto
    adicionando projeto no git conforme aula 20.07

---
+++ app.js
@@ -0,0 +1,80 @@
+const todoInput = document.querySelector('.nova-task');
+const todoBtn = document.querySelector('.adicionar');
+const todoList = document.querySelector('.todo-list');
+
+
+todoBtn.addEventListener('click', adicionarTodo);
+todoList.addEventListener('click', deletarTodo)
+
+function adicionarTodo (event){
+    event.preventDefault();
+
+    if (todoInput.value == ""){
+        alert('Você não pode adicionar uma tarefa em branco!')
+    }
+    else{
+        const divTodo = document.createElement('div');
+        divTodo.classList.add('todo');
+
+        const novoTodo = document.createElement('li');
+        novoTodo.innerText = todoInput.value;
+        novoTodo.classList.add('todo-item');
+        divTodo.appendChild(novoTodo);
+
+        salvarLocalStoreTodo(todoInput.value)
+
+        //botao check
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class ="fas fa-check-circle"></i>';
+        completedButton.classList.add('completed-btn');
+        divTodo.appendChild(completedButton);
+        
+        //botoes excluir
+        const botaoDescarte = document.createElement('button');
+        botaoDescarte.innerHTML = '<i class ="fas fa-trash-alt"></i>';
+        botaoDescarte.classList.add('trash-btn');
+        divTodo.appendChild(botaoDescarte);
+
+        todoList.appendChild(divTodo);
+
+        todoInput.value = ""
+     
+    }
+}
+function deletarTodo(e){
+
+  const itemClicado = e.target;
+
+  if (itemClicado.classList[0] === 'trash-btn'){
+    const todo =  itemClicado.parentElement;
+    todo.remove()
+  }
+  if (itemClicado.classList[0] === 'completed-btn'){
+    const todo =  itemClicado.parentElement;
+    todo.classList.toggle('completed')
+  }
+}
+
+function salvarLocalStoreTodo(todo){
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = []
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))    
+    }
+
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function buscarTodos(){
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = []
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))    
+    }
+    todos.array.forEach(todo => {
+        
+    });
+}
